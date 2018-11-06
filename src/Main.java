a6050e197290b0ca5e9690177e3589b6ef9bb0ff
tangdefeng <tdfrhy@foxmail.com>
2018-11-06 16:48:59 +0800

    修改

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(LeetCoodeString[] args){
+    public static void main(String[] args){
         Solution solution=new Solution();
         System.out.println(  solution.isPalindrome(-10));
     }
