2fd61c8b0c4d39906402a8be1cd5486118b9f8af
tangdefeng <tdfrhy@foxmail.com>
2018-11-14 14:53:34 +0800

    反转整数

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args){
         Solution solution=new Solution();
         ReverseNum reverseNum=new ReverseNum();
+        InsertArea insertArea=new InsertArea();
+        int a[]={1,3,5,6};
+        System.out.println(insertArea.searchInsert(a,7));
         System.out.println(reverseNum.reverse(
                 -1123));
         System.out.println(  solution.isPalindrome(-10));
